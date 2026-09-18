2dfd718d806997f9e6027585b6f37501eb4bfc9b
Comical <330938299+ComicalMC@users.noreply.github.com>
2026-09-18 21:15:30 +0500

    fix spelling mistake

--- src/main/java/client/Minecraft.java
+++ src/main/java/client/Minecraft.java
@@ -38,7 +38,7 @@ public class Minecraft {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE); // Required for MacOS support
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE); // Required for macOS support
 
         window = glfwCreateWindow(854, 480, "Minecraft (LWJGL 3 + Java 25)", NULL, NULL);
         if (window == NULL) {
